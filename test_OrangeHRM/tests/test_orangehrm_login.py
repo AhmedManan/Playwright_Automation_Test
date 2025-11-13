6f04f1d71e8be5a6864025f55435a07fe4e58bf1
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2025-11-14 04:46:16 +0600

    Data Driven Login Test
    * Updated the test_orangehrm_login.py script for data driven testing
    * Added login_data.csv file

--- test_OrangeHRM/tests/test_orangehrm_login.py
+++ test_OrangeHRM/tests/test_orangehrm_login.py
@@ -1,14 +1,24 @@
 import re
+import pytest
 from playwright.sync_api import Page, expect
 from ..pages.login_page import LoginPage
 from ..pages.dashboard_page import DashboardPage
 
+def get_csv_data() -> list:
+    import csv
+    data = []
+    with open("./test_OrangeHRM/test_data/login_data.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
 
-def test_login(page: Page) -> None:
+@pytest.mark.parametrize("username, password", get_csv_data())
+def test_login(page: Page, username, password) -> None:
     login_page = LoginPage(page)
     dashboard_page = DashboardPage(page)
 
     page.goto("https://opensource-demo.orangehrmlive.com/")
-    login_page.login('Admin', 'admin123')
+    login_page.login(username, password)
     dashboard_page.is_dashboard_visible()
 
