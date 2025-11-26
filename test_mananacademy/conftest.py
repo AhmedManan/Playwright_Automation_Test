6e677fcfd73e480d7a86956215f6eb74cd6bf1d1
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2025-11-26 19:24:50 +0600

    Updated headless mode for github actions
    * Update in conftest for github actions

--- test_mananacademy/conftest.py
+++ test_mananacademy/conftest.py
@@ -1,9 +1,11 @@
-import pytest
+import pytest, os
 from playwright.sync_api import sync_playwright
 
 
 @pytest.fixture(scope="session")
 def browser():
+    # Headless=True for CI (GitHub Actions) or check an env variable
+    is_ci = os.getenv('CI', 'false') == 'true'
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=False)
         yield browser
